51c1238ee4fd9e90143758144ea1d18b444d3994
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-23 01:37:51 +0200

    Add files via upload

---
+++ A. Black Square.cpp
@@ -0,0 +1,29 @@
+﻿//Link Problem==>https://codeforces.com/contest/431/problem/A
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pi 3.141592653589793238
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+int counter[1000001] = {};
+int main()
+{
+    ahmed
+        ll tot, a[5];
+    cin >> a[1], a[2], a[3], a[4];
+    string s;
+    cin >> s;
+    for (auto c : s);
+    tot += a[c - '0'];
+    cout << tot << endl;
+
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف
